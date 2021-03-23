4c6e0034d3bfa6b03397e9e63b356d55cc71ecdc
Nick Kallen <nickkallen@me.com>
2021-03-23 11:16:18 +0100

    Creating script to generate code

---
+++ build/generate/src/generate.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+'use strict';
+
+// const path = require('path');
+// const fse = require('fs-extra');
+// const os = require('os');
+// const util = require('./util');
+// const cp = require('child_process');
+// const ejs = require('ejs');
+
