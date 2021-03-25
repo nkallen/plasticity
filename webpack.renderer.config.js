8c8459c225e70e94a43684cdb2a27bc7da14ee80
Nick Kallen <nickkallen@me.com>
2021-03-25 13:32:15 +0100

    small changes

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -2,16 +2,23 @@ const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
 
 rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+    test: /\.css$/,
+    use: [
+        {
+            loader: 'style-loader',
+        },
+        {
+            loader: 'css-loader',
+        },
+    ],
 });
 
 module.exports = {
-  module: {
-    rules,
-  },
-  plugins: plugins,
-  resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
-  },
+    module: {
+        rules,
+    },
+    plugins: plugins,
+    resolve: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.css']
+    },
 };
