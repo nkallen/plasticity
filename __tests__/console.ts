9101cab3325675a9b5088280c1ca3fe7135b6007
Nick Kallen <nickkallen@me.com>
2021-04-15 10:58:31 +0200

    run all tests in parallel - it's slightly faster but also stresses some correctness issues

--- __tests__/console.ts
+++ __tests__/console.ts
@@ -7,3 +7,9 @@ global.console = {
     debug: console.debug,
     assert: console.assert,
 };
+
+import '../src/types/c3d-enum'
+import license from '../license-key.json';
+import c3d from '../build/Release/c3d.node';
+
+c3d.Enabler.EnableMathModules(license.name, license.key);
