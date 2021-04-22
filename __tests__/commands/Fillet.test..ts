e25cdd26d09b2cf45cf31d9f5beea7d05ccccd9b
Nick Kallen <nickkallen@me.com>
2021-04-22 16:22:43 -0400

    minimal fillet and sphere tests

---
+++ __tests__/commands/Fillet.test..ts
@@ -0,0 +1,38 @@
+import * as THREE from "three";
+import BoxFactory from "../../src/commands/box/BoxFactory";
+import { EditorSignals } from '../../src/Editor';
+import { GeometryDatabase } from '../../src/GeometryDatabase';
+import MaterialDatabase from '../../src/MaterialDatabase';
+import { FakeMaterials } from "../../__mocks__/FakeMaterials";
+import FakeSignals from '../../__mocks__/FakeSignals';
+import '../matchers';
+import * as visual from '../../src/VisualModel';
+import FilletFactory from "../../src/commands/fillet/FilletFactory";
+
+let db: GeometryDatabase;
+let makeBox: BoxFactory;
+let makeFillet: FilletFactory;
+let materials: Required<MaterialDatabase>;
+let signals: EditorSignals;
+
+beforeEach(() => {
+    materials = new FakeMaterials();
+    signals = FakeSignals();
+    db = new GeometryDatabase(materials, signals);
+    makeBox = new BoxFactory(db, materials, signals);
+})
+
+describe('commit', () => {
+    test('invokes the appropriate c3d commands', () => {
+        makeBox.p1 = new THREE.Vector3();
+        makeBox.p2 = new THREE.Vector3(1, 0, 0);
+        makeBox.p3 = new THREE.Vector3(1, 1, 0);
+        makeBox.p4 = new THREE.Vector3(1, 1, 1);
+        const box = makeBox.commit() as visual.Solid;
+        const edge = box.edges.children[0] as visual.CurveEdge;
+
+        makeFillet.item = box;
+        makeFillet.edges = [edge];
+        makeFillet.commit();
+    })
+})
---
+++ __tests__/commands/Sphere.test.ts
@@ -0,0 +1,35 @@
+import * as THREE from "three";
+import SphereFactory from "../../src/commands/sphere/SphereFactory";
+import { EditorSignals } from '../../src/Editor';
+import { GeometryDatabase } from '../../src/GeometryDatabase';
+import MaterialDatabase from '../../src/MaterialDatabase';
+import * as visual from '../../src/VisualModel';
+import { FakeMaterials } from "../../__mocks__/FakeMaterials";
+import FakeSignals from '../../__mocks__/FakeSignals';
+import '../matchers';
+
+let db: GeometryDatabase;
+let makeSphere: SphereFactory;
+let materials: Required<MaterialDatabase>;
+let signals: EditorSignals;
+
+beforeEach(() => {
+    materials = new FakeMaterials();
+    signals = FakeSignals();
+    db = new GeometryDatabase(materials, signals);
+    makeSphere = new SphereFactory(db, materials, signals);
+})
+
+describe('commit', () => {
+    test('invokes the appropriate c3d commands', () => {
+        makeSphere.center = new THREE.Vector3();
+        makeSphere.radius = 1;
+        const item = makeSphere.commit() as visual.SpaceItem;
+        const bbox = new THREE.Box3().setFromObject(item);
+        const center = new THREE.Vector3();
+        bbox.getCenter(center);
+        expect(center).toApproximatelyEqual(new THREE.Vector3());
+        expect(bbox.min).toApproximatelyEqual(new THREE.Vector3(-1, -1, -1));
+        expect(bbox.max).toApproximatelyEqual(new THREE.Vector3(1, 1, 1));
+    })
+})
