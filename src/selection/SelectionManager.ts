dbe77768cd256b1089d20fc26983b668f09f829f
Nick Kallen <nickkallen@me.com>
2021-06-08 17:01:22 +0200

    namespacing

--- src/selection/SelectionManager.ts
+++ src/selection/SelectionManager.ts
@@ -6,43 +6,42 @@ import { Clone, SelectionMemento } from '../History';
 import MaterialDatabase from '../MaterialDatabase';
 import { RefCounter } from '../util/Util';
 import * as visual from '../VisualModel';
-import { Curve3D, CurveEdge, Face, PlaneInstance, Region, Solid, SpaceInstance } from '../VisualModel';
 import { Hoverable } from './Hover';
 import { SelectionMode } from './SelectionInteraction';
 
 export interface HasSelection {
     readonly mode: ReadonlySet<SelectionMode>;
-    readonly selectedSolids: ReadonlySet<Solid>;
-    readonly selectedEdges: ReadonlySet<CurveEdge>;
-    readonly selectedFaces: ReadonlySet<Face>;
-    readonly selectedRegions: ReadonlySet<PlaneInstance<Region>>;
-    readonly selectedCurves: ReadonlySet<SpaceInstance<Curve3D>>;
+    readonly selectedSolids: ReadonlySet<visual.Solid>;
+    readonly selectedEdges: ReadonlySet<visual.CurveEdge>;
+    readonly selectedFaces: ReadonlySet<visual.Face>;
+    readonly selectedRegions: ReadonlySet<visual.PlaneInstance<visual.Region>>;
+    readonly selectedCurves: ReadonlySet<visual.SpaceInstance<visual.Curve3D>>;
     hover?: Hoverable;
     readonly selectedChildren: RefCounter<visual.SpaceItem>;
 }
 
 export interface ModifiesSelection {
-    deselectFace(object: Face, parentItem: Solid): void;
-    selectFace(object: Face, parentItem: Solid): void;
-    deselectRegion(object: PlaneInstance<Region>): void;
-    selectRegion(object: PlaneInstance<Region>): void;
-    deselectEdge(object: CurveEdge, parentItem: Solid): void;
-    selectEdge(object: CurveEdge, parentItem: Solid): void;
-    deselectSolid(solid: Solid): void;
-    selectSolid(solid: Solid): void;
-    deselectCurve(curve: SpaceInstance<Curve3D>): void;
-    selectCurve(curve: SpaceInstance<Curve3D>): void;
+    deselectFace(object: visual.Face, parentItem: visual.Solid): void;
+    selectFace(object: visual.Face, parentItem: visual.Solid): void;
+    deselectRegion(object: visual.PlaneInstance<visual.Region>): void;
+    selectRegion(object: visual.PlaneInstance<visual.Region>): void;
+    deselectEdge(object: visual.CurveEdge, parentItem: visual.Solid): void;
+    selectEdge(object: visual.CurveEdge, parentItem: visual.Solid): void;
+    deselectSolid(solid: visual.Solid): void;
+    selectSolid(solid: visual.Solid): void;
+    deselectCurve(curve: visual.SpaceInstance<visual.Curve3D>): void;
+    selectCurve(curve: visual.SpaceInstance<visual.Curve3D>): void;
     deselectAll(): void;
 }
 
 export class SelectionManager implements HasSelection, ModifiesSelection {
     readonly mode = new Set<SelectionMode>([SelectionMode.Solid, SelectionMode.Edge, SelectionMode.Curve, SelectionMode.Face]);
 
-    readonly selectedSolids = new Set<Solid>();
-    readonly selectedEdges = new Set<CurveEdge>();
-    readonly selectedFaces = new Set<Face>();
-    readonly selectedRegions = new Set<PlaneInstance<Region>>();
-    readonly selectedCurves = new Set<SpaceInstance<Curve3D>>();
+    readonly selectedSolids = new Set<visual.Solid>();
+    readonly selectedEdges = new Set<visual.CurveEdge>();
+    readonly selectedFaces = new Set<visual.Face>();
+    readonly selectedRegions = new Set<visual.PlaneInstance<visual.Region>>();
+    readonly selectedCurves = new Set<visual.SpaceInstance<visual.Curve3D>>();
 
     // selectedChildren is the set of solids that have actively selected topological items;
     // It's used in selection logic -- you can't select a solid if its face is already selected, for instance;
@@ -59,7 +58,7 @@ export class SelectionManager implements HasSelection, ModifiesSelection {
         signals.objectRemoved.add(item => this.delete(item));
     }
 
-    deselectFace(object: Face, parentItem: Solid) {
+    deselectFace(object: visual.Face, parentItem: visual.Solid) {
         const model = this.db.lookupTopologyItem(object); // FIXME it would be better to not lookup anything
         this.selectedFaces.delete(object);
         object.material = this.materials.lookup(model);
@@ -67,7 +66,7 @@ export class SelectionManager implements HasSelection, ModifiesSelection {
         this.signals.objectDeselected.dispatch(object);
     }
 
-    selectFace(object: Face, parentItem: Solid) {
+    selectFace(object: visual.Face, parentItem: visual.Solid) {
         const model = this.db.lookupTopologyItem(object); // FIXME it would be better to not lookup anything
         this.hover?.dispose();
         this.hover = undefined;
@@ -78,13 +77,13 @@ export class SelectionManager implements HasSelection, ModifiesSelection {
         this.signals.objectSelected.dispatch(object);
     }
 
-    deselectRegion(object: PlaneInstance<Region>) {
+    deselectRegion(object: visual.PlaneInstance<visual.Region>) {
         this.selectedRegions.delete(object);
         object.material = this.materials.region();
         this.signals.objectDeselected.dispatch(object);
     }
 
-    selectRegion(object: PlaneInstance<Region>) {
+    selectRegion(object: visual.PlaneInstance<visual.Region>) {
         const model = this.db.lookup(object);
         this.hover?.dispose();
         this.hover = undefined;
@@ -93,7 +92,7 @@ export class SelectionManager implements HasSelection, ModifiesSelection {
         this.signals.objectSelected.dispatch(object);
     }
 
-    deselectEdge(object: CurveEdge, parentItem: Solid) {
+    deselectEdge(object: visual.CurveEdge, parentItem: visual.Solid) {
         const model = this.db.lookupTopologyItem(object); // FIXME it would be better to not lookup anything
         this.selectedEdges.delete(object);
         object.material = this.materials.lookup(model);
@@ -101,7 +100,7 @@ export class SelectionManager implements HasSelection, ModifiesSelection {
         this.signals.objectDeselected.dispatch(object);
     }
 
-    selectEdge(object: CurveEdge, parentItem: Solid) {
+    selectEdge(object: visual.CurveEdge, parentItem: visual.Solid) {
         const model = this.db.lookupTopologyItem(object) as c3d.CurveEdge; // FIXME it would be better to not lookup anything
         this.hover?.dispose();
         this.hover = undefined;
@@ -112,26 +111,26 @@ export class SelectionManager implements HasSelection, ModifiesSelection {
         this.signals.objectSelected.dispatch(object);
     }
 
-    deselectSolid(solid: Solid) {
+    deselectSolid(solid: visual.Solid) {
         this.selectedSolids.delete(solid);
         this.signals.objectDeselected.dispatch(solid);
     }
 
-    selectSolid(solid: Solid) {
+    selectSolid(solid: visual.Solid) {
         this.hover?.dispose();
         this.hover = undefined;
         this.selectedSolids.add(solid);
         this.signals.objectSelected.dispatch(solid);
     }
 
-    deselectCurve(curve: SpaceInstance<Curve3D>) {
+    deselectCurve(curve: visual.SpaceInstance<visual.Curve3D>) {
         const model = this.db.lookup(curve);
         this.selectedCurves.delete(curve);
         curve.material = this.materials.line(model);
         this.signals.objectDeselected.dispatch(curve);
     }
 
-    selectCurve(curve: SpaceInstance<Curve3D>) {
+    selectCurve(curve: visual.SpaceInstance<visual.Curve3D>) {
         const model = this.db.lookup(curve);
         this.hover?.dispose();
         this.hover = undefined;
