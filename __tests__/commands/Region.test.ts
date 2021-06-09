79c255ea9b4b35eb175deb78ac3304f670405882
Nick Kallen <nickkallen@me.com>
2021-06-09 11:22:45 +0200

    unit test for region booleans

--- __tests__/commands/Region.test.ts
+++ __tests__/commands/Region.test.ts
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import CircleFactory from "../../src/commands/circle/CircleFactory";
+import { RegionBooleanFactory } from "../../src/commands/region/RegionBooleanFactory";
 import RegionFactory from "../../src/commands/region/RegionFactory";
 import { EditorSignals } from '../../src/Editor';
 import { GeometryDatabase } from '../../src/GeometryDatabase';
@@ -10,8 +11,10 @@ import FakeSignals from '../../__mocks__/FakeSignals';
 import '../matchers';
 
 let db: GeometryDatabase;
-let makeCircle: CircleFactory;
-let makeRegion: RegionFactory;
+let makeCircle1: CircleFactory;
+let makeCircle2: CircleFactory;
+let makeRegion1: RegionFactory;
+let makeRegion2: RegionFactory;
 let materials: MaterialDatabase;
 let signals: EditorSignals;
 
@@ -19,24 +22,68 @@ beforeEach(() => {
     materials = new FakeMaterials();
     signals = FakeSignals();
     db = new GeometryDatabase(materials, signals);
-    makeCircle = new CircleFactory(db, materials, signals);
-    makeRegion = new RegionFactory(db, materials, signals);
+    makeCircle1 = new CircleFactory(db, materials, signals);
+    makeCircle2 = new CircleFactory(db, materials, signals);
+    makeRegion1 = new RegionFactory(db, materials, signals);
+    makeRegion2 = new RegionFactory(db, materials, signals);
 })
 
-describe('commit', () => {
+describe('create regions', () => {
     test('invokes the appropriate c3d commands', async () => {
-        makeCircle.center = new THREE.Vector3();
-        makeCircle.radius = 1;
-        const circle = await makeCircle.commit() as visual.SpaceInstance<visual.Curve3D>;
-        makeRegion.contours = [circle];
-        const items = await makeRegion.commit() as visual.PlaneInstance<visual.Region>;
-        const item = items[0];
+        makeCircle1.center = new THREE.Vector3();
+        makeCircle1.radius = 1;
+        const circle = await makeCircle1.commit() as visual.SpaceInstance<visual.Curve3D>;
 
-        const bbox = new THREE.Box3().setFromObject(item);
+        makeRegion1.contours = [circle];
+        const regions = await makeRegion1.commit() as visual.PlaneInstance<visual.Region>[];
+        expect(regions.length).toBe(1);
+        const region = regions[0];
+
+        const bbox = new THREE.Box3().setFromObject(region);
         const center = new THREE.Vector3();
         bbox.getCenter(center);
         expect(center).toApproximatelyEqual(new THREE.Vector3());
         expect(bbox.min).toApproximatelyEqual(new THREE.Vector3(-1, -1, 0));
         expect(bbox.max).toApproximatelyEqual(new THREE.Vector3(1, 1, 0));
+    })
+})
+
+describe('boolean regions', () => {
+    let regionBoolean: RegionBooleanFactory;
+
+    beforeEach(() => {
+        regionBoolean = new RegionBooleanFactory(db, materials, signals);
+    });
+
+    test('invokes the appropriate c3d commands', async () => {
+        makeCircle1.center = new THREE.Vector3(0.5, 0, 0);
+        makeCircle1.radius = 1;
+        const circle1 = await makeCircle1.commit() as visual.SpaceInstance<visual.Curve3D>;
+
+        makeCircle2.center = new THREE.Vector3(-0.5, 0, 0);
+        makeCircle2.radius = 1;
+        const circle2 = await makeCircle2.commit() as visual.SpaceInstance<visual.Curve3D>;
+
+        makeRegion1.contours = [circle1];
+        const regions1 = await makeRegion1.commit() as visual.PlaneInstance<visual.Region>[];
+        expect(regions1.length).toBe(1);
+        const region1 = regions1[0];
+
+        makeRegion2.contours = [circle2];
+        const regions2 = await makeRegion2.commit() as visual.PlaneInstance<visual.Region>[];
+        expect(regions2.length).toBe(1);
+        const region2 = regions2[0];
+
+        regionBoolean.regions = [region1, region2];
+        const boolean = await regionBoolean.commit() as visual.PlaneInstance<visual.Region>[];
+        expect(boolean.length).toBe(1);
+        const item = boolean[0];
+
+        const bbox = new THREE.Box3().setFromObject(item);
+        const center = new THREE.Vector3();
+        bbox.getCenter(center);
+        expect(center).toApproximatelyEqual(new THREE.Vector3());
+        expect(bbox.min).toApproximatelyEqual(new THREE.Vector3(-0.5, -0.87, 0));
+        expect(bbox.max).toApproximatelyEqual(new THREE.Vector3(0.5, 0.87, 0));
     })
 })
--- src/commands/region/RegionFactory.ts
+++ src/commands/region/RegionFactory.ts
@@ -19,7 +19,7 @@ export default class RegionFactory extends GeometryFactory {
             const inst = this.db.lookup(contour);
             const item = inst.GetSpaceItem();
             const curve = item.Cast<c3d.Curve3D>(c3d.SpaceType.Curve3D);
-            const { curve2d, placement } = curve.GetPlaneCurve(true);
+            const { curve2d, placement } = curve.GetPlaneCurve(false);
             const origin = placement.GetOrigin();
             placement.SetOrigin(new c3d.CartPoint3D(0,0,0));
             placement_ = placement;
@@ -40,38 +40,3 @@ export default class RegionFactory extends GeometryFactory {
     }
 }
 
-export class RegionBooleanFactory extends GeometryFactory {
-    regions = new Array<visual.PlaneInstance<visual.Region>>();
-
-    async doUpdate() {
-    }
-
-    async doCommit() {
-        const input = [];
-        let placement: c3d.Placement3D;
-        for (const region of this.regions) {
-            const inst = this.db.lookup(region);
-            const item = inst.GetPlaneItem();
-            const r = item.Cast<c3d.Region>(c3d.PlaneType.Region);
-            input.push(r);
-            placement = inst.GetPlacement();
-        }
-
-        const params = new c3d.RegionBooleanParams(c3d.RegionOperationType.Intersect, true);
-        const { regions } = c3d.ActionRegion.CreateBooleanResultRegions(input[0], input[1], params);
-
-        const result = [];
-        for (const region of regions) {
-            result.push(this.db.addItem(new c3d.PlaneInstance(region, placement!)));
-        }
-        const final = await Promise.all(result);
-
-        for (const region of this.regions) {
-            this.db.removeItem(region);
-        }
-        return final;
-    }
-
-    doCancel() {
-    }
-}
