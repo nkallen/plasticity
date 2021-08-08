4eccc02b1b1c3fc6970febb482f53d714514ac07
Nick Kallen <nickkallen@me.com>
2021-08-08 14:42:12 +0200

    file move

--- __tests__/selection/SelectionManager.test.ts
+++ __tests__/selection/SelectionManager.test.ts
@@ -1,16 +1,16 @@
 import * as THREE from 'three';
-import BoxFactory from '../src/commands/box/BoxFactory';
-import { CenterCircleFactory } from '../src/commands/circle/CircleFactory';
-import LineFactory from '../src/commands/line/LineFactory';
-import { RegionFactory } from '../src/commands/region/RegionFactory';
-import { EditorSignals } from '../src/editor/EditorSignals';
-import { GeometryDatabase } from '../src/editor/GeometryDatabase';
-import MaterialDatabase from '../src/editor/MaterialDatabase';
-import * as visual from '../src/editor/VisualModel';
-import { SelectionInteractionManager } from '../src/selection/SelectionInteraction';
-import { SelectionManager } from '../src/selection/SelectionManager';
-import { FakeMaterials } from "../__mocks__/FakeMaterials";
-import './matchers';
+import BoxFactory from '../../src/commands/box/BoxFactory';
+import { CenterCircleFactory } from '../../src/commands/circle/CircleFactory';
+import LineFactory from '../../src/commands/line/LineFactory';
+import { RegionFactory } from '../../src/commands/region/RegionFactory';
+import { EditorSignals } from '../../src/editor/EditorSignals';
+import { GeometryDatabase } from '../../src/editor/GeometryDatabase';
+import MaterialDatabase from '../../src/editor/MaterialDatabase';
+import * as visual from '../../src/editor/VisualModel';
+import { SelectionInteractionManager } from '../../src/selection/SelectionInteraction';
+import { SelectionManager } from '../../src/selection/SelectionManager';
+import { FakeMaterials } from "../../__mocks__/FakeMaterials";
+import '../matchers';
 
 let db: GeometryDatabase;
 let materials: MaterialDatabase;
