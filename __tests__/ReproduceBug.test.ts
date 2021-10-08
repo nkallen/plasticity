5f11fbdc6c60ff6804911ce6538f432d55295b1b
Nick Kallen <nickkallen@me.com>
2021-10-08 19:04:56 +0200

    todos

--- __tests__/ReproduceBug.test.ts
+++ __tests__/ReproduceBug.test.ts
@@ -39,7 +39,29 @@ test.skip("this crashes", async () => {
     const fn = new c3d.CubicFunction(1, 1);
     const edgeFunction = new c3d.EdgeFunction(edge, fn);
     const edgeFunctions = [edgeFunction];
-    
+
     const names = new c3d.SNameMaker(c3d.CreatorType.FilletSolid, c3d.ESides.SideNone, 0);
     c3d.ActionSolid.FilletSolid(solid, c3d.CopyMode.Copy, edgeFunctions, [], params, names);
+});
+
+
+test.skip("bad fillet", async () => {
+    const filePath_in = "/Users/nickkallen/Downloads/bad-draft-solid.c3d";
+    const data = await fs.promises.readFile(filePath_in);
+    await db.deserialize(data);
+    const solid = db.find(visual.Solid)[0].model;
+    const filePath_out = `/Users/nickkallen/Downloads/bad-draft-solid.out.c3d`;
+    const faces = [solid.GetFace(10)!];
+    const placement = new c3d.Placement3D();
+    const pivot = new c3d.CartPoint3D(191.60838865000276, -103.65056230484981, 145.81608281885392);
+    placement.SetOrigin(pivot);
+    placement.SetAxisZ(new c3d.Vector3D(0, 1, 0));
+    placement.Reset();
+    const angle = 0.5;
+    const names = new c3d.SNameMaker(c3d.CreatorType.DraftSolid, c3d.ESides.SideNone, 0);
+    const drafted = c3d.ActionSolid.DraftSolid(solid, c3d.CopyMode.Copy, placement, angle, faces, c3d.FacePropagation.All, false, names);
+    const model = new c3d.Model();
+    model.AddItem(drafted, 1010);
+    const { memory } = await c3d.Writer.WriteItems_async(model);
+    await fs.promises.writeFile(filePath_out, memory);
 });
