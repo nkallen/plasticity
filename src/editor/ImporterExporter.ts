d837770cda0db42665a43e548317a5eed8501eee
Nick Kallen <nickkallen@me.com>
2021-10-08 18:41:10 +0200

    fix importer buig

--- src/editor/ImporterExporter.ts
+++ src/editor/ImporterExporter.ts
@@ -1,12 +1,11 @@
 import * as fs from 'fs';
 import c3d from '../../build/Release/c3d.node';
-import Command from '../commands/Command';
+import Command, * as cmd from '../commands/Command';
 import { ExportCommand } from "../commands/CommandLike";
-import { DatabaseLike } from "./GeometryDatabase";
-import * as cmd from "../commands/Command";
+import { GeometryDatabase } from "./GeometryDatabase";
 
 interface EditorLike extends cmd.EditorLike {
-    db: DatabaseLike,
+    _db: GeometryDatabase,
     enqueue(command: Command, interrupt?: boolean): Promise<void>;
 }
 
@@ -14,18 +13,18 @@ export class ImporterExporter {
     constructor(private readonly editor: EditorLike) { }
 
     async open(filePaths: string[]) {
-        const { editor: { db } } = this;
+        const { editor: { _db } } = this;
         for (const filePath of filePaths) {
             if (/\.c3d$/.test(filePath)) {
                 const data = await fs.promises.readFile(filePath);
-                await db.deserialize(data);
+                await _db.deserialize(data);
             } else {
                 const { result, model } = await c3d.Conversion.ImportFromFile_async(filePath);
                 if (result !== c3d.ConvResType.Success) {
                     console.error(filePath, c3d.ConvResType[result]);
                     continue;
                 }
-                await db.load(model);
+                await _db.load(model);
             }
         }
     }
