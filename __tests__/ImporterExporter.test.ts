e9d89f1837656eedbb029eb12b9f95a3f9a5a794
Nick Kallen <nickkallen@me.com>
2021-10-08 15:52:48 +0200

    fixed test on windows

--- __tests__/ImporterExporter.test.ts
+++ __tests__/ImporterExporter.test.ts
@@ -35,10 +35,11 @@ test("export & import c3d", async () => {
     makeSphere.center = new THREE.Vector3();
     makeSphere.radius = 1;
     const item = await makeSphere.commit() as visual.Solid;
+    const model = db.saveToMemento().model;
+
     await db.removeItem(item);
     expect(db.visibleObjects.length).toBe(0);
     
-    const model = db.saveToMemento().model;
     const dir = os.tmpdir();
     const filePath = path.join(dir, 'export.c3d');
     await importer.export(model, filePath);
