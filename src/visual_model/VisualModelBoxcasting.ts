c0eafb32d2852e049910774ec17feda9defa4391
Nick Kallen <nickkallen@me.com>
2021-11-22 20:20:56 +0100

    CurveEdge intersection

--- src/visual_model/VisualModelBoxcasting.ts
+++ src/visual_model/VisualModelBoxcasting.ts
@@ -108,6 +108,8 @@ Solids: {
         const { drawRange } = geometry;
         const index = geometry.index!;
 
+        if (this.simpleName != 'face,1,5') return false;
+
         const position = geometry.attributes.position;
 
         _frustum.copy(boxcaster.frustum);
@@ -120,7 +122,9 @@ Solids: {
         for (let i = start; i < end; i++) {
             const j = index.getX(i);
             _v.fromBufferAttribute(position, j);
-            if (_frustum.containsPoint(_v)) return true;
+            if (_frustum.containsPoint(_v)) {
+                return true;
+            }
         }
         return false;
     }
@@ -204,18 +208,23 @@ Solids: {
         const { group } = this;
 
         const instanceStart = geometry.attributes.instanceStart as THREE.InterleavedBufferAttribute;
+        const instanceEnd = geometry.attributes.instanceEnd; // camera forward is negative
         const array = instanceStart.data.array as Float32Array;
 
         _frustum.copy(boxcaster.frustum);
         _inverseMatrix.copy(matrixWorld).invert();
         _frustum.applyMatrix4(_inverseMatrix);
 
-        const start = group.start / 3;
-        const end = (group.start + group.count) / 3;
+        const start = group.start / 3 / 2;
+        const end = (group.start + group.count) / 3 / 2;
 
         for (let i = start; i <= end; i++) {
-            _v.set(array[3 * i + 0], array[3 * i + 1], array[3 * i + 2]);
-            if (_frustum.containsPoint(_v)) return true;
+            _line.start.fromBufferAttribute(instanceStart, i);
+            _line.end.fromBufferAttribute(instanceEnd, i);
+
+            if (_frustum.containsPoint(_line.start)) return true;
+            if (_frustum.containsPoint(_line.end)) return true;
+            if (_frustum.intersectsLine(_line)) return true;
         }
         return false;
     }
@@ -240,13 +249,21 @@ class FastFrustum extends THREE.Frustum {
         }
         return this;
     }
+
+    intersectsLine(line: THREE.Line3): boolean {
+        for (const plane of this.planes) {
+            if (plane.intersectsLine(line)) return true;
+        }
+        return false;
+    }
 }
 
-const _sphere = new THREE.Sphere();
 const _frustum = new FastFrustum();
-const _center = new THREE.Vector3();
+const _start = new THREE.Vector3();
+const _end = new THREE.Vector3();
 const _v = new THREE.Vector3();
 const _inverseMatrix = new THREE.Matrix4();
 const _box = new THREE.Box3();
+const _line = new THREE.Line3();
 
 export { };
