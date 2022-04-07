71d520e6eab7c173fb9b7cd60efb8cb4ad7ba31d
Nick Kallen <nickkallen@me.com>
2022-04-07 15:27:26 +0200

    sketch interface

--- src/components/outliner/Outliner.tsx
+++ src/components/outliner/Outliner.tsx
@@ -64,6 +64,107 @@ export default (editor: Editor) => {
         }
 
         render = () => {
+            const hidden = true, visible = true, selectable = true;
+            const result = <>
+                <div class="px-4 pt-4 pb-3">
+                    <h1 class="text-xs font-bold text-neutral-100">Scene</h1>
+                </div>
+                <div class="px-4">
+                    <div class="flex h-8 p-3 overflow-hidden items-center rounded-md hover:bg-neutral-600 group">
+                        <plasticity-icon name="nav-arrow-right" class="text-neutral-500"></plasticity-icon>
+                        <plasticity-icon name="folder" class="text-neutral-500"></plasticity-icon>
+                        <div class="p-0.5 flex-1">
+                            <input type="text" class="w-full text-neutral-300 text-xs group-hover:text-neutral-100 h-6 p-0.5 bg-transparent rounded pointer-events-none overflow-hidden overflow-ellipsis whitespace-nowrap" placeholder='' value="Collapsed group" disabled autoComplete='no' autocorrect='off' spellCheck={false}></input>
+                        </div>
+                    </div>
+                    <div class="flex gap-2 h-8 p-3 overflow-hidden items-center rounded-md hover:bg-neutral-600 group">
+                        <plasticity-icon name="nav-arrow-down" class="text-neutral-500"></plasticity-icon>
+                        <plasticity-icon name="folder" class="text-neutral-500"></plasticity-icon>
+                        <div class="p-0.5 flex-1">
+                            <input type="text" class="w-full text-neutral-300 text-xs group-hover:text-neutral-100 h-6 p-0.5 bg-transparent rounded pointer-events-none overflow-hidden overflow-ellipsis whitespace-nowrap" placeholder='' value="Outer group" disabled autoComplete='no' autocorrect='off' spellCheck={false}></input>
+                        </div>
+                    </div>
+                    <div class="ml-3 flex gap-2 h-8 p-3 overflow-hidden items-center rounded-md hover:bg-neutral-600 group">
+                        <plasticity-icon name="nav-arrow-right" class="text-neutral-500"></plasticity-icon>
+                        <plasticity-icon name="folder" class="text-neutral-500"></plasticity-icon>
+                        <div class="p-0.5 flex-1">
+                            <input type="text" class="w-full text-neutral-300 text-xs group-hover:text-neutral-100 h-6 p-0.5 bg-transparent rounded pointer-events-none overflow-hidden overflow-ellipsis whitespace-nowrap" placeholder='' value="Inner group with extremely long name" disabled autoComplete='no' autocorrect='off' spellCheck={false}></input>
+                        </div>
+                        <button class="p-1 rounded group text-neutral-300 group-hover:block hidden">
+                            <plasticity-icon key={!hidden} name={!hidden ? 'eye' : 'eye-off'}></plasticity-icon>
+                        </button>
+                        <button class="p-1 rounded group text-neutral-300 group-hover:block hidden">
+                            <plasticity-icon key={visible} name={visible ? 'light-bulb-on' : 'light-bulb-off'}></plasticity-icon>
+                        </button>
+                        <button class="p-1 rounded group text-neutral-300 group-hover:block hidden">
+                            <plasticity-icon key={selectable} name={selectable ? 'no-lock' : 'lock'}></plasticity-icon>
+                        </button>
+                    </div>
+                    <div class="ml-3 flex gap-2 h-8 p-3 overflow-hidden items-center rounded-md hover:bg-neutral-600 group">
+                        <plasticity-icon name="nav-arrow-down" class="text-neutral-500"></plasticity-icon>
+                        <plasticity-icon name="folder-solids" class="text-neutral-500"></plasticity-icon>
+                        <div class="p-0.5 flex-1">
+                            <input type="text" class="w-full text-neutral-300 text-xs group-hover:text-neutral-100 h-6 p-0.5 bg-transparent rounded pointer-events-none overflow-hidden overflow-ellipsis whitespace-nowrap" placeholder='' value="Solids" disabled autoComplete='no' autocorrect='off' spellCheck={false}></input>
+                        </div>
+                    </div>
+                    <div class="ml-9 flex gap-2 h-8 p-3 overflow-hidden items-center rounded-md bg-accent-600 hover:bg-accent-500 group">
+                        <plasticity-icon name="solid" class="text-accent-100 hover:text-accent-50"></plasticity-icon>
+                        <div class="p-0.5 flex-1">
+                            <input type="text" class="select-text w-full text-neutral-900 text-xs h-6 p-0.5 rounded overflow-hidden overflow-ellipsis whitespace-nowrap" placeholder='Solid' value="Cylinder.003" autoComplete='no' autocorrect='off' spellCheck={false}></input>
+                        </div>
+                    </div>
+                    <div class="ml-9 flex gap-2 h-8 p-3 overflow-hidden items-center rounded-md hover:bg-neutral-600 group">
+                        <plasticity-icon name="solid" class="text-accent-500"></plasticity-icon>
+                        <div class="p-0.5 flex-1">
+                            <input type="text" class="w-full text-neutral-300 text-xs group-hover:text-neutral-100 h-6 p-0.5 bg-transparent rounded pointer-events-none overflow-hidden overflow-ellipsis whitespace-nowrap" placeholder='' value="Sphere.001" disabled autoComplete='no' autocorrect='off' spellCheck={false}></input>
+                        </div>
+                    </div>
+                    <div class="ml-9 flex gap-2 h-8 p-3 overflow-hidden items-center rounded-md bg-accent-600 hover:bg-accent-500 group">
+                        <plasticity-icon name="solid" class="text-accent-100 hover:text-accent-50"></plasticity-icon>
+                        <div class="p-0.5 flex-1">
+                            <input type="text" class="w-full text-accent-100 hover:text-accent-50 text-xs h-6 p-0.5 bg-transparent rounded pointer-events-none overflow-hidden overflow-ellipsis whitespace-nowrap" placeholder='' value="Cylinder.002" disabled autoComplete='no' autocorrect='off' spellCheck={false}></input>
+                        </div>
+                    </div>
+                    <div class="ml-9 flex gap-2 h-8 p-3 overflow-hidden items-center rounded-md hover:bg-neutral-600 group">
+                        <plasticity-icon name="solid" class="text-accent-500"></plasticity-icon>
+                        <div class="p-0.5 flex-1">
+                            <input type="text" class="w-full text-neutral-200 group-hover:text-neutral-100 text-xs h-6 p-0.5 bg-transparent rounded pointer-events-none overflow-hidden overflow-ellipsis whitespace-nowrap" placeholder='' value="Cylinder.002" disabled autoComplete='no' autocorrect='off' spellCheck={false}></input>
+                        </div>
+                    </div>
+                    <div class="ml-3 flex gap-2 h-8 p-3 overflow-hidden items-center rounded-md hover:bg-neutral-600 group">
+                        <plasticity-icon name="nav-arrow-down" class="text-neutral-500"></plasticity-icon>
+                        <plasticity-icon name="folder-curves" class="text-neutral-500"></plasticity-icon>
+                        <div class="p-0.5 flex-1">
+                            <input type="text" class="w-full text-neutral-300 text-xs group-hover:text-neutral-100 h-6 p-0.5 bg-transparent rounded pointer-events-none overflow-hidden overflow-ellipsis whitespace-nowrap" placeholder='' value="Curves" disabled autoComplete='no' autocorrect='off' spellCheck={false}></input>
+                        </div>
+                    </div>
+                    <div class="ml-9 flex gap-2 h-8 p-3 overflow-hidden items-center rounded-md hover:bg-neutral-600 group">
+                        <plasticity-icon name="curve" class="text-accent-500"></plasticity-icon>
+                        <div class="p-0.5 flex-1">
+                            <input type="text" class="w-full text-neutral-300 text-xs group-hover:text-neutral-100 h-6 p-0.5 bg-transparent rounded pointer-events-none overflow-hidden overflow-ellipsis whitespace-nowrap" placeholder='' value="Circle.002" disabled autoComplete='no' autocorrect='off' spellCheck={false}></input>
+                        </div>
+                    </div>
+                    <div class="ml-9 flex gap-2 h-8 p-3 overflow-hidden items-center rounded-md hover:bg-neutral-600 group">
+                        <plasticity-icon name="curve" class="text-accent-500"></plasticity-icon>
+                        <div class="p-0.5 flex-1">
+                            <input type="text" class="w-full text-neutral-300 text-xs group-hover:text-neutral-100 h-6 p-0.5 bg-transparent rounded pointer-events-none overflow-hidden overflow-ellipsis whitespace-nowrap" placeholder='' value="Rectangle.002" disabled autoComplete='no' autocorrect='off' spellCheck={false}></input>
+                        </div>
+                        <button class="p-1 rounded group text-neutral-300 group-hover:visible invisible hover:text-neutral-100">
+                            <plasticity-icon key={!hidden} name={!hidden ? 'eye' : 'eye-off'}></plasticity-icon>
+                        </button>
+                        <button class="p-1 rounded group text-neutral-300 group-hover:visible invisible hover:text-neutral-100">
+                            <plasticity-icon key={visible} name={visible ? 'light-bulb-on' : 'light-bulb-off'}></plasticity-icon>
+                        </button>
+                        <button class="p-1 rounded group text-neutral-300 group-hover:visible invisible hover:text-neutral-100">
+                            <plasticity-icon key={selectable} name={selectable ? 'no-lock' : 'lock'}></plasticity-icon>
+                        </button>
+                    </div>
+                </div>
+            </>;
+            render(result, this);
+        }
+
+        render2 = () => {
             const { scene: { groups, groups: { root }, nodes } } = editor;
             const flattened = flatten(root, groups, this.expandedGroups);
             const map = new Map<NodeKey, RefObject<any>>();
--- src/components/toolbar/Icon.tsx
+++ src/components/toolbar/Icon.tsx
@@ -526,6 +526,36 @@ export default (editor: Editor) => {
                         <svg width="24" height="24" class="w-4 h-4 stroke-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>, this);
+                case 'nav-arrow-down':
+                    return render(
+                        <svg width="24" height="24" class="w-4 h-4 stroke-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 9L12 15L18 9" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>, this);
+                case 'nav-arrow-right':
+                    return render(
+                        <svg width="24" height="24" class="w-4 h-4 stroke-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9 6L15 12L9 18" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>, this);
+                case 'folder':
+                    return render(
+                        <svg width="24" height="24" class="w-4 h-4 stroke-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M2 11V4.6C2 4.26863 2.26863 4 2.6 4H8.77805C8.92127 4 9.05977 4.05124 9.16852 4.14445L12.3315 6.85555C12.4402 6.94876 12.5787 7 12.722 7H21.4C21.7314 7 22 7.26863 22 7.6V11M2 11V19.4C2 19.7314 2.26863 20 2.6 20H21.4C21.7314 20 22 19.7314 22 19.4V11M2 11H22" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>, this);
+                case 'folder-solids':
+                    return render(
+                        <svg width="24" height="24" class="w-4 h-4 stroke-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M22 12.6V20.4C22 20.7314 21.7314 21 21.4 21H13.6C13.2686 21 13 20.7314 13 20.4V12.6C13 12.2686 13.2686 12 13.6 12H21.4C21.7314 12 22 12.2686 22 12.6Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M19.5 14.51L19.51 14.4989" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M13 18.2L16.5 17L22 19" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M2 10V3.6C2 3.26863 2.26863 3 2.6 3H8.77805C8.92127 3 9.05977 3.05124 9.16852 3.14445L12.3315 5.85555C12.4402 5.94876 12.5787 6 12.722 6H21.4C21.7314 6 22 6.26863 22 6.6V9M2 10V18.4C2 18.7314 2.26863 19 2.6 19H10M2 10H10" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>, this);
+                case 'folder-curves':
+                    return render(
+                        <svg width="24" height="24" class="w-4 h-4 stroke-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M22 12.6V20.4C22 20.7314 21.7314 21 21.4 21H13.6C13.2686 21 13 20.7314 13 20.4V12.6C13 12.2686 13.2686 12 13.6 12H21.4C21.7314 12 22 12.2686 22 12.6Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M16.918 14.5737C16.5184 14.324 16 14.6113 16 15.0825V17.9175C16 18.3887 16.5184 18.676 16.918 18.4263L19.1859 17.0088C19.5619 16.7738 19.5619 16.2262 19.1859 15.9912L16.918 14.5737Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M2 10V3.6C2 3.26863 2.26863 3 2.6 3H8.77805C8.92127 3 9.05977 3.05124 9.16852 3.14445L12.3315 5.85555C12.4402 5.94876 12.5787 6 12.722 6H21.4C21.7314 6 22 6.26863 22 6.6V9M2 10V18.4C2 18.7314 2.26863 19 2.6 19H10M2 10H10" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>, this);
                 default:
                     console.warn(`${this.name} is missing icon`);
             }
