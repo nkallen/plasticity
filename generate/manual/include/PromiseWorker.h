0449c628de0b2832967e73836b4ba131385e4336
Nick Kallen <nickkallen@me.com>
2022-07-04 13:53:30 +0200

    c++ Format

--- generate/manual/include/PromiseWorker.h
+++ generate/manual/include/PromiseWorker.h
@@ -4,21 +4,20 @@
 
 class PromiseWorker : public Napi::AsyncWorker {
 public:
-    PromiseWorker(Napi::Promise::Deferred const &d, const char* resource_name) : AsyncWorker(d.Env(), resource_name), deferred(d) {}
-    PromiseWorker(Napi::Promise::Deferred const &d) : AsyncWorker(d.Env()), deferred(d) {}
+  PromiseWorker(Napi::Promise::Deferred const &d, const char *resource_name)
+      : AsyncWorker(d.Env(), resource_name), deferred(d) {}
+  PromiseWorker(Napi::Promise::Deferred const &d)
+      : AsyncWorker(d.Env()), deferred(d) {}
 
-    virtual void Resolve(Napi::Promise::Deferred const &deferred) = 0;
+  virtual void Resolve(Napi::Promise::Deferred const &deferred) = 0;
 
-    void OnOK() override {
-        Resolve(deferred);
-    }
+  void OnOK() override { Resolve(deferred); }
 
-    void OnError(Napi::Error const &error) override {
-        Reject(deferred, error);
-    }
+  void OnError(Napi::Error const &error) override { Reject(deferred, error); }
 
-    virtual void Reject(Napi::Promise::Deferred const &deferred, Napi::Error const &error) = 0;
+  virtual void Reject(Napi::Promise::Deferred const &deferred,
+                      Napi::Error const &error) = 0;
 
 private:
-    Napi::Promise::Deferred deferred;
+  Napi::Promise::Deferred deferred;
 };
