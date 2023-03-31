8a09b693d01261bc8eacde0ff7c7ba6aa0eaf7d2
Farouk Serragedine <farouk.serragedine@gmail.com>
2023-03-31 17:39:48 +0200

    Refactor and improve code organization and consistency
    Removed unused imports: Removed the import for the 'underscore' library since it was not being used in the code.
    Ensured consistent use of single quotes for string literals: Updated all string literals to use single quotes consistently for better readability and code style consistency.

--- generate/src/generate.mjs
+++ generate/src/generate.mjs
@@ -17,11 +17,7 @@ import cp from 'child_process';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Parse a description of the API.
-
 const declarations = Parse(api);
-
-// Load some templates.
 
 const templates = {
     binding: util.readLocalFile('templates/binding.gyp'),
@@ -31,15 +27,13 @@ const templates = {
     module_header: util.readLocalFile('templates/module_header.h'),
     module_content: util.readLocalFile('templates/module_content.cc'),
     type_declaration: util.readLocalFile('templates/c3d.d.ts'),
-}
+};
+
 for (const k in templates) {
     templates[k] = ejs.compile(templates[k], {
         views: [path.join(__dirname, "../templates/partials")]
     });
 }
-
-// We generate the code first into /tmp, then sync everything back to our project
-// in lib/c3d
 
 const tempDirPath = path.join(os.tmpdir(), 'c3d');
 const tempSrcDirPath = path.join(tempDirPath, 'src');
@@ -48,20 +42,13 @@ const tempIncludeDirPath = path.join(tempDirPath, 'include');
 const finalSrcDirPath = path.join(__dirname, '../../lib/c3d/src');
 const finalIncludeDirPath = path.join(__dirname, '../../lib/c3d/include');
 
-// While most things are automatically generated, some things are done by hand.
-
 await fse.copy(path.resolve(__dirname, '../manual/include'), tempIncludeDirPath);
 await fse.copy(path.resolve(__dirname, '../manual/src'), tempSrcDirPath);
-
-// First start with 'binding' and 'index'. They describe how to build the project,
-// cf node-gyp documentation for details.
 
 util.writeLocalFile('../binding.gyp', beautify(templates.binding({ classes: declarations })), 'binding.gyp');
 util.writeLocalFile('../lib/c3d/index.cc', beautify(templates.index({ classes: declarations })), 'index.cc');
 
 util.writeLocalFile('../lib/c3d/c3d.d.ts', beautify(templates.type_declaration({ classes: declarations })), "c3d.d.ts");
-
-// Auto-generate the c++ files from the api description
 
 for (const klass of declarations) {
     if (klass.ignore) continue;
@@ -77,8 +64,6 @@ for (const klass of declarations) {
         klass.cppClassName + '.cc');
 }
 
-// Pretty-print the c++ just so we can debug more easily (optional)
-
 const astyle = (process.platform == 'darwin' || process.platform == 'linux') ? cp.execSync('command -v astyle') : cp.execSync('where astyle');
 if (astyle) {
     cp.execSync(
@@ -89,8 +74,6 @@ if (astyle) {
         'rm ' + tempSrcDirPath + '/*.cc.orig ' + tempIncludeDirPath +
         '/*.h.orig');
 }
-
-// Sync from /tmp into lib/c3d
 
 await util.syncDirs(tempSrcDirPath, finalSrcDirPath);
 await util.syncDirs(tempIncludeDirPath, finalIncludeDirPath);
